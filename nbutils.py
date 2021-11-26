97af7ed841a6e75b9a6987384866270e7d2c593d
MohammadReza Ebrahimi <mamareza.e@gmail.com>
2021-11-25 23:16:44 -0500

    corrects code formatting

--- nbutils.py
+++ nbutils.py
@@ -1,27 +1,27 @@
-import nbformat
 import re
-
-from PIL import Image
 from base64 import b64decode
 from io import BytesIO
-from IPython.display import display
+
+import nbformat
 import numpy as np
+from PIL import Image
 
 
 def read_nb(file_path):
     return nbformat.read(file_path, as_version=nbformat.NO_CONVERT)
+
 
 def iter_cell(nb, clue=None, cell_type='any'):
     CELL_TYPES = ['markdown', 'code']
     if cell_type == 'any':
         cell_type = CELL_TYPES
     else:
-        assert cell_type in CELL_TYPES, f'{cell_type} not in {CELL_TYPES}' 
+        assert cell_type in CELL_TYPES, f'{cell_type} not in {CELL_TYPES}'
         cell_type = [cell_type]
-    
-    clue = clue or '' 
+
+    clue = clue or ''
     return (
-        cell 
+        cell
         for cell in nb['cells']
         if cell['cell_type'] in cell_type and clue in cell['source']
     )
@@ -30,21 +30,26 @@ def iter_cell(nb, clue=None, cell_type='any'):
 def filter_between(str, before, after):
     if not str:
         return None
-    
+
     pattern = re.escape(before) + '(.*?)' + re.escape(after)
     if ans := re.search(pattern, str, flags=re.S):
         return ans.group(1)
 
+
 def filter_code(code, remove_comment=True, remove_indent=True):
     if not code:
         return None
-    
+
     result = []
     for line in code.split('\n'):
-        if remove_comment: line = line.split('#')[0] 
-        if remove_indent: line = line.strip()
-        if line: result.append(line)
+        if remove_comment:
+            line = line.split('#')[0]
+        if remove_indent:
+            line = line.strip()
+        if line:
+            result.append(line)
     return '\n'.join(result)
+
 
 def search_nb(nb_path, cell_clue, before, after, remove_comment=True, remove_indent=True):
     nb = read_nb(nb_path)
@@ -52,18 +57,22 @@ def search_nb(nb_path, cell_clue, before, after, remove_comment=True, remove_ind
     for cell in iter_cell(nb, cell_clue):
         code = cell['source']
         code = filter_between(code, before, after)
-        code = filter_code(code, remove_comment=remove_comment, remove_indent=remove_indent)
+        code = filter_code(code, remove_comment=remove_comment,
+                           remove_indent=remove_indent)
         if code:
             result.append(code)
     return result
 
+
 def b64_to_image(img_b64):
     return Image.open(BytesIO(b64decode(img_b64)))
 
+
 def whiten_backgroud(image):
     new_image = Image.new("RGBA", image.size, "WHITE")
-    new_image.paste(image, (0, 0), image)             
+    new_image.paste(image, (0, 0), image)
     return new_image.convert('RGB')
+
 
 def get_cell_b64images(cell):
     images = []
@@ -78,23 +87,23 @@ def get_cell_b64images(cell):
 def vstack_imgs(images):
     if not images:
         return []
-    min_shape = sorted([(np.sum(i.size), i.size ) for i in images])[0][1]
+    min_shape = sorted([(np.sum(i.size), i.size) for i in images])[0][1]
     images_resized = [img.resize(min_shape) for img in images]
     imgs_comb = np.vstack(images_resized)
     return Image.fromarray(imgs_comb)
+
 
 def get_cell_images(cell, concat=False, remove_transparency=False):
     images = get_cell_b64images(cell)
     if not images:
         return None
-    
+
     images = [b64_to_image(img) for img in images]
-    
+
     if remove_transparency:
         images = [whiten_backgroud(img) for img in images]
-    
+
     if concat:
         images = vstack_imgs(images)
-    
-    return images 
-    
+
+    return images
